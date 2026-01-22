31183339348a9c81e3ce56f646593316e0c17011
guillermopickman-spec <guillermopickman@gmail.com>
2026-01-22 01:09:15 +0100

    Fix development servers and add BookingStatus enum

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/': {
-                target: 'http://127.0.0.1:8001',
+                target: 'http://127.0.0.1:8000',
                 changeOrigin: true,
             },
         },
